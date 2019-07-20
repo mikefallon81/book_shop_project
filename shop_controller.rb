68a1694aae678dbab7893399779ad2ef6277df39
mikefallon <mikefallon81@gmail.com>
2019-07-20 20:24:11 +0100

    index page created and products displaying

--- shop_controller.rb
+++ shop_controller.rb
@@ -1,7 +1,13 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require_relative('models/authors')
+require_relative('models/author')
 require_relative('models/genre')
 require_relative('models/product')
 require_relative('models/publisher')
 also_reload( './models/*' )
+
+
+get '/products' do
+  @products = Product.all
+  erb(:index)
+end
