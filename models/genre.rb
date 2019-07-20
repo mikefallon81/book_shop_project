fd995c1aef323ddfc118182a117997fc02ff6229
mikefallon <mikefallon81@gmail.com>
2019-07-20 12:46:11 +0100

    Genre Class and save method complete

---
+++ models/genre.rb
@@ -0,0 +1,24 @@
+require_relative('../db/sql_runner')
+
+class Genre
+
+  attr_reader :id
+  attr_accessor :genre_type
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @genre_type = options['genre_type']
+  end
+
+  def save()
+    sql = "INSERT INTO genres(
+    genre_type
+    )
+    VALUES ($1)
+    RETURNING id;"
+    values = [@genre_type]
+    genre = SqlRunner.run(sql, values)
+    @id = genre.first()['id'].to_i
+  end
+
+end
