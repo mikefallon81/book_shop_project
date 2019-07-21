79eaa843ad6330cd53d83dbf30b42d61b5980f23
mikefallon <mikefallon81@gmail.com>
2019-07-21 09:58:14 +0100

    view files created

--- shop_controller.rb
+++ shop_controller.rb
@@ -6,8 +6,13 @@ require_relative('models/product')
 require_relative('models/publisher')
 also_reload( './models/*' )
 
-
+#INDEX
 get '/products' do
   @products = Product.all
   erb(:index)
+end
+
+#CREATE
+get 'products/new' do
+  @product = Product.new()
 end
